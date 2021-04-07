b9a60451c00353fbd5bdc52ac30ddc427ddfa395
mohora11 <mohora@naver.com>
2021-04-07 16:17:59 +0900

    2021.04.07

---
+++ src/p06/lecture/p4method/MyClass3.java
@@ -0,0 +1,19 @@
+package p06.lecture.p4method;
+
+public class MyClass3 {
+	int a;
+	
+	void method1() {
+//		int a;
+		System.out.println("a:" + this.a);
+		System.out.println("method1 호출");
+		
+		this.method2();
+		
+		System.out.println("method1 진행....");
+	}
+	
+	void method2() {
+		System.out.println("method2 호출");
+	}
+}
