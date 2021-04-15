ccb3605ad662ef141cb3b59261bdb33d2de75668
mohora11 <mohora@naver.com>
2021-04-15 11:31:27 +0900

    2021.04.15

---
+++ src/p08/textbook/ex/ex3/Cat.java
@@ -0,0 +1,9 @@
+package p08.textbook.ex.ex3;
+
+public class Cat implements Soundable {
+	@Override
+	public String sound() {
+		
+		return "야옹";
+	}
+}
---
+++ src/p08/textbook/ex/ex3/Dog.java
@@ -0,0 +1,10 @@
+package p08.textbook.ex.ex3;
+
+public class Dog implements Soundable {
+	@Override
+	public String sound() {
+		
+		return "멍멍";
+		
+	}
+}
