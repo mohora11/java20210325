ea4133e0b9030f2e6a000ccf97603a16830dd0e3
mohora11 <mohora@naver.com>
2021-04-28 16:54:27 +0900

    2021.04.28

--- src/p15/lecture/A05Stack.java
+++ src/p15/lecture/A05Stack.java
@@ -4,21 +4,21 @@ import java.util.Stack;
 
 public class A05Stack {
 	public static void main(String[] args) {
-		Stack<Integer> stack = new Stack<>();
+		Stack<Integer> stack = new Stack<>();//인티저 타입의 새로운 스택을 선언
+		//Stack : 아래서부터 차곡차곡 쌓는다 생각하면 됨
 		
-		stack.push(3);
-		stack.push(5);
+		stack.push(3); //push 문법  Stack에서는 아이템을 넣을려면 push사용
+		stack.push(5); // 첫째로 3, 둘째로 5, 셋째로 6을 넣음
 		stack.push(6);
 		
+				
+		int last = stack.pop(); //Stack pop 문법 가장 마지막 객체
+		System.out.println(last);// 를 불러옴과 동시에 스택에서 제거
 		
-		
-		int last = stack.pop();
+		last = stack.pop(); // 마지막 저장한 객체 이전의 객체를 불러옴
 		System.out.println(last);
 		
-		last = stack.pop();
-		System.out.println(last);
-		
-		last  = stack.pop();
+		last  = stack.pop(); // 여기서는 첫째로 저장한 3을 불러옴
 		System.out.println(last);
 		
 //		last  = stack.pop();
--- src/p15/lecture/A06Queue.java
+++ src/p15/lecture/A06Queue.java
@@ -5,24 +5,25 @@ import java.util.Queue;
 
 public class A06Queue {
 	public static void main(String[] args) {
-		Queue<Integer> queue = new LinkedList<>();
-		
+		Queue<Integer> queue = new LinkedList<>();//인티저 타입의 큐를 선언
+		//큐는 선입선출 
 		queue.offer(9);
 		queue.offer(6);
 		queue.offer(3);
+		System.out.println(queue);
 		
-		int first = queue.poll();
-		
+		int first = queue.poll(); // queue의 poll문법 첫번째 넣었던
+								  // 객체부터 꺼내온다	
 		System.out.println(first);
+		System.out.println(queue.poll()); // 순서대로 6,3 을 꺼내왔다.
 		System.out.println(queue.poll());
-		System.out.println(queue.poll());
-		System.out.println(queue.poll());
+		System.out.println(queue.poll()); // 마지막은 null값
 		
 		queue.offer(30);
 		queue.offer(10);
 		queue.offer(20);
 		
-		System.out.println(queue.peek());
+		System.out.println(queue.peek()); // 맨먼저 넣었던걸 소환한다
 		System.out.println(queue.peek());
 		
 		
