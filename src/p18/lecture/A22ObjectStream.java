551b938f4d135721c98179dabb7e9524c1845860
mohora11 <mohora@naver.com>
2021-05-04 11:19:36 +0900

    ObjectStream

---
+++ src/p18/lecture/A22ObjectStream.java
@@ -0,0 +1,32 @@
+package p18.lecture;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+
+public class A22ObjectStream {
+	public static void main(String[] args) throws Exception  {
+		String path = "src/p18/lecture/output9.txt";
+		FileOutputStream fos = new FileOutputStream(path);
+		ObjectOutputStream oos = new ObjectOutputStream(fos);
+		
+		oos.writeObject(new String("java"));
+		oos.writeObject(new String[] {"css", "html"});
+		
+		oos.close();
+		fos.close();
+		
+		FileInputStream fis = new FileInputStream(path);
+		ObjectInputStream ois = new ObjectInputStream(fis);
+		
+		
+		String s = (String) ois.readObject();
+		String[] arr = (String[]) ois.readObject();
+		System.out.println(s);
+		
+		ois.close();
+		fis.close();
+	}
+}
