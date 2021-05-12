e943df679200bdf94ec7660b3394235b16dcc329
mohora11 <mohora@naver.com>
2021-05-13 08:45:58 +0900

    재수정

--- src/p02/lecture/A12TypeConversion.java
+++ src/p02/lecture/A12TypeConversion.java
@@ -18,7 +18,7 @@ public class A12TypeConversion {
 		// 정수 -> 실수
 		int intVar1 = 3;
 		float floatVar2 = intVar1;  // 자동형변환
-		double doubleVar2 = intvar1;// 자동형변환
+		double doubleVar2 = intVar1;// 자동형변환
 		
 		// 실수 -> 정수
 		double doubleVar3 = 3.14;
